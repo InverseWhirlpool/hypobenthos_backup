7d0f9193d648ca9b3ded9076e859c6f3ce596571
gezihuzi <a495008890@live.com>
2019-05-25 22:05:34 +0800

    Site updated: 2019-05-25 22:05:34

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,11 @@
-<script src="/firebase-app.js"></script>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.4/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
 <script>
+  // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyBRXrsGL7mzRZ48AEqoxGJiLKGZjYyggLI",
     authDomain: "hypobenthos-octofile.firebaseapp.com",
@@ -9,5 +15,6 @@
     messagingSenderId: "698744953118",
     appId: "1:698744953118:web:5db6d29102eb0e96"
   };
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 </script>
