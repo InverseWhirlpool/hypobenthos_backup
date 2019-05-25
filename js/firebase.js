fa172c35f8f4e4183539fcc702f90f66a45eec35
gezihuzi <a495008890@live.com>
2019-05-25 21:50:59 +0800

    Site updated: 2019-05-25 21:50:59

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.4/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBRXrsGL7mzRZ48AEqoxGJiLKGZjYyggLI",
+    authDomain: "hypobenthos-octofile.firebaseapp.com",
+    databaseURL: "https://hypobenthos-octofile.firebaseio.com",
+    projectId: "hypobenthos-octofile",
+    storageBucket: "hypobenthos-octofile.appspot.com",
+    messagingSenderId: "698744953118",
+    appId: "1:698744953118:web:5db6d29102eb0e96"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
